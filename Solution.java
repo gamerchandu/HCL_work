a2098018d21d95a5cf59ab95ac88ab7b8fc8419f
Chandu <chchandu123@gmail.com>
2017-09-13 06:40:41 +0530

    5 C

--- Solution.java
+++ Solution.java
@@ -6,6 +6,7 @@ public static void main(String args[])
 	System.out.println("third commit");
 	System.out.println("Helloworld");
 	System.out.println("four commit");
+	System.out.println("four commit");
 	
 }
 }
